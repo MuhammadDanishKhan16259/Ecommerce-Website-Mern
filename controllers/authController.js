9271fae7796ccaccc58544bd4379b16caec3b0d8
MuhammadDanishKhan16259 <muhammaddanishkhan1999@gmail.com>
2023-06-23 05:06:40 +0500

    dashss

--- controllers/authController.js
+++ controllers/authController.js
@@ -109,6 +109,43 @@ export const loginController = async (req, res) => {
   }
 };
 // export default { registerController };
+//forgotPasswordController
+
+export const forgotPasswordController = async (req, res) => {
+  try {
+    const { email, answer, newPassword } = req.body;
+    if (!email) {
+      res.status(400).send({
+        message: "Email is required",
+      });
+    }
+    if (!answer) {
+      res.status(400).send({
+        message: "answer is required",
+      });
+    }
+    if (!newPassword) {
+      res.status(400).send({
+        message: "New Password is required",
+      });
+    }
+    //check
+    const user = await userModel.findOne({ email, answer });
+
+    //validation
+    if (!user) {
+      // return res
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Something Went Wrong",
+      error,
+    });
+  }
+};
+
 //test controller
 export const testController = (req, res) => {
   try {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,7 @@ import {
   registerController,
   loginController,
   testController,
+  forgotPasswordController,
 } from "../controllers/authController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 //router object
@@ -14,6 +15,8 @@ router.post("/register", registerController);
 
 //LOGIN || POST
 router.post("/login", loginController);
+//Forgot Password
+router.post("/forgot-password", forgotPasswordController);
 
 //test route
 router.get("/test", requireSignIn, isAdmin, testController);
