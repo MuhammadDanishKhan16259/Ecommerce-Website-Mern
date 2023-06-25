a72b6b93e6b9665a4e299dc3b92f251ec6fefaa9
MuhammadDanishKhan16259 <muhammaddanishkhan1999@gmail.com>
2023-06-26 02:36:26 +0500

    product

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/Auth.jsx";
+import "antd/dist/reset.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <App />
